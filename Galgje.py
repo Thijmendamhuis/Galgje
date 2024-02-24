0528b7237f4d94943c5d33bbf54c9b7bcdc670fa
Thijmendamhuis <thijmendamhuis2006@gmail.com>
2024-02-24 18:59:02 +0100

    first commit

--- Galgje.py
+++ Galgje.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 # Globale variabelen om de spelstatus bij te houden
-geraden_letters = []  # Lijst om bij te houden welke letters zijn geraden
+geraden_letters = [] # Lijst om bij te houden welke letters zijn geraden
 punten = 0  # Score van de speler
 levens = 10  # Aantal resterende levens
 geheimwoord_str = ''  # Het geheime woord dat geraden moet worden
@@ -24,6 +24,7 @@ def geheimwoord():
 def check_win():
     # Controleer of alle letters van het geheime woord zijn geraden
     return all(letter in geraden_letters for letter in geheimwoord())
+        
 
 # Functie om de juiste afbeelding van de galg te tonen op basis van het aantal resterende levens
 def nieuwe_foto():
@@ -40,9 +41,8 @@ def scoreboard():
     scoreboard_punten_label.config(text=f'Score: {punten}')
     scoreboard_geradenletters_label.config(text=f'Geraden Letters: ' + str(geraden_letters))
 # Functie om het woord te tonen als het woord niet geraden is 
-def toon_geheimwoord():
-    woord_label.config(text=f"Het woord was: {geheimwoord_str.lower()}")
-    woord_label.grid(padx=100, pady=0)
+
+
 
 # Functie om de GUI bij te werken op basis van de spelstatus
 def update_display(event=None):
@@ -51,6 +51,7 @@ def update_display(event=None):
     display_text = ' '.join(letter if letter in geraden_letters else '_' for letter in geheimwoord())
     # Update het label om het geheime woord en de geraden letters weer te geven
     display_label.config(text=display_text)
+    gok_entry.delete(0, 'end')
     # Verberg widgets die niet meer nodig zijn nadat het spel is gestart
     welkom_frame.grid_forget()
     label.grid_forget()
@@ -63,16 +64,14 @@ def update_display(event=None):
     gok_entry.grid(padx=100, pady=0)
     raad_button.grid(padx=100, pady=0)
     scoreboard_geradenletters_label.grid(padx=100, pady=0)
-    woord_label.grid_forget()
-
-
+    
 # Functie om een nieuw spel te starten
 def nieuwe_game(event=None):
-    global punten, levens, geraden_letters, geheimwoord_str
+    global punten, levens, geheimwoord_str, geraden_letters
     # Reset de score, het aantal levens en de lijst met geraden letters
     punten = 0
     levens = 10
-    geraden_letters = []
+    geraden_letters = [] # Reset de lijst met geraden letters
     # Wis de invoervelden voor het woord en de gok
     woord_entry.delete(0, 'end')
     gok_entry.delete(0, 'end')
@@ -90,8 +89,15 @@ def nieuwe_game(event=None):
     raad_button.grid_forget()
     nieuwe_foto_label.grid_forget()
     nieuwe_game_button.grid_forget()
+    vorige_woord_label.grid_forget()
     geheimwoord_str = ''
     nieuwe_foto()
+
+def toon_geheimwoord():
+    global geraden_letters
+    vorige_woord_label.config(text=f"Het woord was: {geheimwoord_str.lower()}")
+    vorige_woord_label.grid_configure()
+    geraden_letters = []
 
 # Functie om de gok van de speler te verwerken
 def gok(event=None):
@@ -116,8 +122,10 @@ def gok(event=None):
                     punten += 1
                     levens = 10
                     scoreboard()
-                    # Toon de knop voor het starten van een nieuw spel
+                    # Toon de knop voor het starten van een nieuw spel en wat het geheimwoord was
                     nieuwe_game_button.grid_configure()
+                    toon_geheimwoord()
+                      
                 else:
                     # Verlaag het aantal resterende levens als de gok fout is
                     if gok not in geheimwoord_str:
@@ -131,7 +139,11 @@ def gok(event=None):
                     tk.messagebox.showinfo("Gefeliciteerd!", "Je hebt het woord geraden!")
                     punten += 1
                     scoreboard()
+                    # Toon de knop voor het starten van een nieuw spel en wat het geheimwoord was
                     nieuwe_game_button.grid_configure()
+                    toon_geheimwoord()
+                    
+                    
                 else:
                     tk.messagebox.showwarning("Ongeldige invoer", "Het ingevoerde woord is niet correct.")
                     levens -= 1
@@ -144,11 +156,13 @@ def gok(event=None):
         else:
             # Toon een melding als het spel verloren is
             tk.messagebox.showwarning("Game over", "Je hebt helaas geen levens meer over")
-            # Toon de knop voor het starten van een nieuw spel
+            # Toon de knop voor het starten van een nieuw spel en wat het geheimwoord was
             nieuwe_game_button.grid_configure()
             toon_geheimwoord()
+            
+            
     else: 
-        tk.messagebox.showwarning("Waarschuwing", "Je heb deze lette al geprobeerd") 
+        tk.messagebox.showwarning("Waarschuwing", "Je heb deze letter al geprobeerd") 
         return None
     
 # Maak het hoofdvenster van de applicatie
@@ -214,8 +228,7 @@ woord_entry.grid(padx=100, pady=0)
 # Plaats de welkomstknop in het venster
 welkom_button.grid(padx=100, pady=0)
 
-woord_label = tk.Label(root, text="")
-
+vorige_woord_label = tk.Label(root, text='')
 
 
 # Start de hoofdlus van de GUI
